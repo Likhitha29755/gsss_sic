ff0a62ac7a9ab1bf1b8e5193551cf97f6ac04ebf
likhitha <likhitha0920@gmail.com>
2025-08-11 21:24:09 +0530

    this is add p1 and p3 to git

---
+++ day1/p2.py
@@ -0,0 +1,6 @@
+#Program to check if an integer is an Even or not.
+n=int(input('Enter a number: '))
+if n % 2 == 0:
+    print('The given number',n, 'is a Even number.')
+else:
+    print('The given number',n, 'is not a  Even number.')
